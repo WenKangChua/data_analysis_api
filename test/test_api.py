a6846cd3cd8a3d06fa088f33ce1e437c614fdb84
WenKangChua <88809920+WenKangChua@users.noreply.github.com>
2025-12-21 23:17:00 +0800

    Add test for all cases

--- test/test_api.py
+++ test/test_api.py
@@ -4,26 +4,36 @@ import pandas as pd
 
 url = "http://127.0.0.1:8000/"
 
-end_point_1 = "/analysis/trend"
-
-params = {
+# ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- 
+end_point_1 = "/analysis/sample"
+params_1 = {
+    "limit": 2
+}
+# ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- 
+end_point_2 = "/analysis/summary_stats"
+params_2 = {
+    # "town": "TAMPINES",
+    # "flat_type": "5 ROOM",
+    # "flat_model": "Improved",
+    # "floor_area_sqm": 105.0,
+    # "remaining_lease_years": 720,
+    # "storey": 12,
+    "year": 2025,
+    "month_num": 11
+}
+# ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- 
+end_point_3 = "/analysis/trend"
+params_3 = {
     "town": "TAMPINES",
     "flat_type": "5 ROOM",
     # "flat_model": "Improved",
     # "floor_area_sqm": 105.0,
     # "remaining_lease_years": 720,
-    # "storey": 34,
-    # "year": 2025,
-    # "month_num": 5
+    # "storey": 34
 }
-
-# test_1 = requests.get(url + end_point_1, params=params).json()
-# print(json.dumps(test_1, indent=4))
-
 # ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- 
-
-end_point_2 = "/analysis/predict"
-payload = {
+end_point_4 = "/analysis/predict"
+payload_4 = {
     "town": "TAMPINES",
     "flat_type": "5 ROOM",
     "flat_model": "Model A",
@@ -31,8 +41,29 @@ payload = {
     "remaining_lease_years": 90,
     "storey": 10,
     "year": 2026,
-    "month_num": 1
+    "month_num": 3
 }
+# ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- 
 
-test_2 = requests.post(url + end_point_2, json=payload).json()
-print(json.dumps(test_2, indent=4))
+# /analysis/sample
+sample = requests.get(url + end_point_1, params=params_1).json()
+print("sample:")
+print(json.dumps(sample, indent=4))
+print("\n")
+
+# /analysis/summary_stats
+summary_stats = requests.get(url + end_point_2, params=params_2).json()
+print("summary_stats:")
+print(json.dumps(summary_stats, indent=4))
+print("\n")
+
+# /analysis/trend
+trend = requests.get(url + end_point_3, params=params_3).json()
+print("trend:")
+print(json.dumps(trend, indent=4))
+print("\n")
+
+# /analysis/predict
+predict = requests.post(url + end_point_4, json=payload_4).json()
+print("predict:")
+print(json.dumps(predict, indent=4))
