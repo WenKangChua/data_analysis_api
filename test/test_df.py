50f01930615ecfd6c5896b25064686d213f6f9d9
WenKangChua <88809920+WenKangChua@users.noreply.github.com>
2025-12-22 06:05:42 +0000

    Initialise

--- test/test_df.py
+++ test/test_df.py
@@ -2,4 +2,5 @@ import pandas as pd
 from production.data import *
 
 hdb_resale_data = load_hdb_resale_data()
-print(hdb_resale_data.head(5))
+print(hdb_resale_data.sample(5))
+print(hdb_resale_data.info())
